30b98b0eba5a471cce8d497c41576ece8c068932
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-03-22 13:26:04 +0000

    Add ability to add books

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+let submitButton = document.getElementById("subBtn");
+let form =  document.getElementById("myForm");
+let appendTo = document.querySelector(".bookContainer");
+submitButton.addEventListener("click", () => addBookToLibrary());
+form.addEventListener('submit', handleForm);
+console.log(appendTo);
 let myLibrary = [];
+
+function handleForm(event)
+{
+    event.preventDefault();
+}
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,4 +21,17 @@ function Book(title, author, pages, read) {
 function addBookToLibrary() 
 {
 
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#auth").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.getElementById("read").checked;
+
+    let book = new Book([title],[author],[pages],[read]);
+    myLibrary.push(book);
+    let newBook =  document.createElement('div');
+    newBook.textContent = `${book.title + book.author + book.pages + book.read}`;
+    
+    newBook.classList.add("bookCard");
+    appendTo.appendChild(newBook);   
 }
+
