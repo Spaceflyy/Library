ed392146393552754cfac04b05e5929dc0c27807
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-03-24 17:06:58 +0000

    Add button to show add book menu

--- main.js
+++ main.js
@@ -20,17 +20,31 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() 
 {
-
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#auth").value;
     let pages = document.querySelector("#pages").value;
-    let read = document.getElementById("read").checked;
+    let read = document.getElementById("read").checked ? "Read" : "Not Read";
 
     let book = new Book([title],[author],[pages],[read]);
     myLibrary.push(book);
     let newBook =  document.createElement('div');
-    newBook.textContent = `${book.title + book.author + book.pages + book.read}`;
-    
+
+    for (const [key, value] of Object.entries(book))
+    {
+        let test;
+        if(key == "title")
+        {
+            test = document.createElement('h2');
+        }else
+        {
+            test = document.createElement('p');
+
+        }
+        test.textContent = value;
+        newBook.appendChild(test);
+    }
+
+
     newBook.classList.add("bookCard");
     appendTo.appendChild(newBook);   
 }
