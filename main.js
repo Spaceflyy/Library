15a5b5d89ce71a7345ded603197c14c547631e08
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-03-25 19:16:57 +0000

    Add removing of books from array and updating the visuals to reflect it

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 let submitButton = document.getElementById("subBtn");
 let form =  document.getElementById("myForm");
 let appendTo = document.querySelector(".bookContainer");
+let bookbtn = document.querySelector(".addBookBtn");
+let addForm = document.querySelector(".addBookForm");
+let closeBtn = document.querySelector("#closeBtn");
+
 submitButton.addEventListener("click", () => addBookToLibrary());
+bookbtn.addEventListener("click", () => showAddMenu())
+closeBtn.addEventListener("click", () =>showAddMenu());
+
+// closeCardBtn.forEach(item =>{
+//     item.addEventListener("click", deleteCard)
+// })
 form.addEventListener('submit', handleForm);
-console.log(appendTo);
+
+
 let myLibrary = [];
+
 
 function handleForm(event)
 {
     event.preventDefault();
+}
+
+function showAddMenu()
+{
+    if(addForm.style.display == "none" || addForm.style.display == "")
+    {
+        addForm.style.display = "block";
+    } else
+    {
+        addForm.style.display = "none";
+    }
 }
 
 function Book(title, author, pages, read) {
@@ -24,28 +47,75 @@ function addBookToLibrary()
     let author = document.querySelector("#auth").value;
     let pages = document.querySelector("#pages").value;
     let read = document.getElementById("read").checked ? "Read" : "Not Read";
-
     let book = new Book([title],[author],[pages],[read]);
     myLibrary.push(book);
-    let newBook =  document.createElement('div');
+
+    createCard(book);
+
+
+ 
+}
+
+function createCard(book)
+{
+    bookbtn.style.display = "none";
+    let closeBtn = document.createElement("IMG");
+    let newBook =  document.createElement('div');  
+    let closeBtnParent = document.createElement("div")
+    closeBtnParent.setAttribute("id", "closeBtnCard");
+    closeBtn.classList.add("icon");
+    closeBtn.setAttribute("src", "./icons/close.svg");
+    closeBtnParent.appendChild(closeBtn);
+    newBook.appendChild(closeBtnParent);
 
     for (const [key, value] of Object.entries(book))
     {
-        let test;
+        let element;
         if(key == "title")
         {
-            test = document.createElement('h2');
+            element = document.createElement('h2');
         }else
         {
-            test = document.createElement('p');
+            element = document.createElement('p');
 
         }
-        test.textContent = value;
-        newBook.appendChild(test);
+        element.textContent = value;
+        newBook.appendChild(element);
     }
 
 
     newBook.classList.add("bookCard");
-    appendTo.appendChild(newBook);   
+    newBook.setAttribute("data", myLibrary.indexOf(book));
+    appendTo.appendChild(newBook);  
+
+    closeBtnParent.addEventListener("click",deleteCard);
+
+
+
 }
 
+function deleteCard(e)
+{
+    let indexOfCard = e.target.parentNode.parentNode.getAttribute("data");
+    myLibrary.splice(indexOfCard, 1);
+    let elements = document.querySelectorAll(".bookCard");
+    elements.forEach(card => {
+        card.remove();
+    });
+    updateLibrary(myLibrary);
+}
+
+function updateLibrary(library)
+{
+
+    if(library.length == 0)
+    {
+        bookbtn.style.display = "flex";
+    }
+
+
+    console.log(library.length);
+    library.forEach(book => {
+        createCard(book);
+    });
+}
