aaa7a5f2f59ecf56ceebef18c1f0628c2d1ea6fa
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-03-28 18:54:19 +0100

    Add functionality to mark as read button

--- main.js
+++ main.js
@@ -4,19 +4,37 @@ let appendTo = document.querySelector(".bookContainer");
 let bookbtn = document.querySelector(".addBookBtn");
 let addForm = document.querySelector(".addBookForm");
 let closeBtn = document.querySelector("#closeBtn .icon");
+let bookBtnHeader = document.querySelector(".header button");
+
 
 submitButton.addEventListener("click", () => addBookToLibrary());
-bookbtn.addEventListener("click", () => showAddMenu())
+bookbtn.addEventListener("click", () => showAddMenu());
+bookBtnHeader.addEventListener("click", () => showAddMenu());
 closeBtn.addEventListener("click", () =>showAddMenu());
 
-// closeCardBtn.forEach(item =>{
-//     item.addEventListener("click", deleteCard)
-// })
 form.addEventListener('submit', handleForm);
 
 
 let myLibrary = [];
 
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.toggleRead = function () {
+    console.log(this.read);
+
+    if(this.read == "Not Read")
+    {
+        this.read = "Read";
+    } else
+    {
+        this.read = "Not Read";
+    }
+}
 
 function handleForm(event)
 {
@@ -34,88 +52,117 @@ function showAddMenu()
     }
 }
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+
 
 function addBookToLibrary() 
 {
+    bookbtn.style.display = "none";
+
     let title = document.querySelector("#title").value;
-    let author = document.querySelector("#auth").value;
+    let author= document.querySelector("#auth").value;
     let pages = document.querySelector("#pages").value;
+
     let read = document.getElementById("read").checked ? "Read" : "Not Read";
+
     let book = new Book([title],[author],[pages],[read]);
     myLibrary.push(book);
-
     createCard(book);
 
-
- 
 }
 
 function createCard(book)
 {
-    bookbtn.style.display = "none";
-    let closeBtn = document.createElement("IMG");
-    let newBook =  document.createElement('div');  
-    let closeBtnParent = document.createElement("div")
-    closeBtnParent.setAttribute("id", "closeBtnCard");
-    closeBtn.classList.add("icon");
-    closeBtn.setAttribute("src", "./icons/close.svg");
-    closeBtnParent.appendChild(closeBtn);
-    newBook.appendChild(closeBtnParent);
+    // Create card elements
+    let card = document.createElement("div");
+    let closeBtnParent = document.createElement("div");
+    let closeBtnCard = document.createElement("IMG");
+    let readIcon = document.createElement("IMG");
 
-    for (const [key, value] of Object.entries(book))
+    // Add classes to card elements
+    card.classList.add("bookCard");
+    closeBtnCard.classList.add("icon");
+    readIcon.classList.add("icon");
+
+    // Set attributes for cards
+    closeBtnParent.setAttribute("id", "closeBtn");
+    if(book.read == "Not Read")
+    {  
+        readIcon.setAttribute("src", "./icons/read.svg");
+
+    } else
     {
-        let element;
-        if(key == "title")
-        {
-            element = document.createElement('h2');
-        }else
-        {
-            element = document.createElement('p');
+        readIcon.setAttribute("src", "./icons/unread.svg");
 
-        }
-        element.textContent = value;
-        newBook.appendChild(element);
     }
 
+    
+    closeBtnCard.setAttribute("src", "./icons/close.svg");
+    closeBtnCard.addEventListener("click", deleteCard);
+    readIcon.addEventListener("click", markAsRead);
+    card.setAttribute("data", myLibrary.indexOf(book));
 
-    newBook.classList.add("bookCard");
-    newBook.setAttribute("data", myLibrary.indexOf(book));
-    appendTo.appendChild(newBook);  
+    // Append icons so they appear at the top
+    closeBtnParent.appendChild(readIcon);
+    closeBtnParent.appendChild(closeBtnCard);
+    card.appendChild(closeBtnParent);
 
-    closeBtn.addEventListener("click",deleteCard);
-
-
-
+    Object.entries(book).forEach(([key, value]) => {
+        let bookInfo;
+        if(key == "title")
+        {
+             bookInfo = document.createElement("h2");
+        } else
+        {
+             bookInfo = document.createElement("p");
+        }
+        bookInfo.innerHTML =  `${value}`;
+        card.appendChild(bookInfo);
+    });
+    
+    appendTo.appendChild(card);
 }
 
 function deleteCard(e)
 {
     let indexOfCard = e.target.parentNode.parentNode.getAttribute("data");
     myLibrary.splice(indexOfCard, 1);
-    let elements = document.querySelectorAll(".bookCard");
-    elements.forEach(card => {
-        card.remove();
-    });
     updateLibrary(myLibrary);
+}
+
+function markAsRead(e)
+{
+    let indexOfCard =  e.target.parentNode.parentNode.getAttribute("data");
+
+    let book = myLibrary[indexOfCard];
+    if(book.read == "Not Read")
+    {  
+        e.target.setAttribute("src", "./icons/unread.svg");
+    } else
+    {
+        e.target.setAttribute("src", "./icons/read.svg");
+
+    }
+
+    book.toggleRead();
+    updateLibrary(myLibrary);
+
 }
 
 function updateLibrary(library)
 {
 
+    let elements = document.querySelectorAll(".bookCard");
+    elements.forEach(card => {
+        card.remove();
+    });
+
     if(library.length == 0)
     {
         bookbtn.style.display = "flex";
-    }
+    } 
+    
 
-
-    console.log(library.length);
     library.forEach(book => {
-        createCard(book);
-    });
+         createCard(book);
+     });
 }
